c9a0b999f9de98fda522f1bacfd27beb1d8bb209
Starliege <urrianif@gmail.com>
2022-08-03 16:35:44 +0200

    bonus done

--- team.js
+++ team.js
@@ -1,4 +1,4 @@
-// Utilizzando i dati forniti, creare un array di oggetti per rappresentare i membri del team. Ogni membro è caratterizzato dalle seguenti informazioni: nome, ruolo e foto.
+// Utilizzando i dati forniti, creare un array di oggetti per rappresentare i membri del team. Og è caratterizzato dalle seguenti informazioni: nome, ruolo e foto.
 
 // MILESTONE 0:
 // Creare l’array di oggetti con le informazioni fornite.
@@ -42,7 +42,11 @@ const team = [
 
 //  MILESTONE 2:
 // Stampare le stesse informazioni su DOM sottoforma di stringhe
-  
+// BONUS 1:
+// Trasformare la stringa foto in una immagine effettiva
+// BONUS 2:
+// Organizzare i singoli membri in card/schede
+
   let teamContainer = document.querySelector(".team-container");
 
   for (key in team) {
@@ -61,3 +65,4 @@ const team = [
     </div>
     `;
 }
+
